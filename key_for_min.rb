9c0077c2d3463c3950661b9abe8f8668d9266000
Kevin Iturralde <keviniturralde3@gmail.com>
2020-08-18 13:43:09 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(hash)
   lowest_value= nil
   hash.each do |k, v|
     if lowest_value ==nil || v < lowest_value
-      lowest_value == v
-      lowest_key == k
+      lowest_value = v
+      lowest_key = k
     end
   end
 
